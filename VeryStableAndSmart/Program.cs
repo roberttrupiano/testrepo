14bb75bbe015d03ae5f46d80a02ead9c56d4302e
Robert Trupiano <rtrupiano@utsi.net>
2018-01-10 14:31:20 -0500

    Add project files.

---
+++ VeryStableAndSmart/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace VeryStableAndSmart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                //test
+                Console.WriteLine("Get Fucked Nerd!");
+                Thread.Sleep(1000);
+            }
+            
+        }
+    }
+}
